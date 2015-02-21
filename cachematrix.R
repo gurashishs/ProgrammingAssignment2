e76ef4dc70caab4a779dd182ea5e69a9d6585dea
Gurashish Singh <gurashishs@gmail.com>
2015-02-20 22:25:15 -0500

    Finished the functions for optimizing large matrix inversions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below are to help solve for the inverses of 
+## large matrices. These functions make the process more efficient
+## by caching the solution for the inverse
 
-## Write a short comment describing this function
+## Desc: Creates a special object with a matrix and its inverse, 
+## each with getters and setters.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	xInv <- NULL
+	set <- function(xNew){
+		x <- xNew
+		xInv <- NULL
+	}
+	get <- function() x
+	setInverse <- function(xInvNew) xInv <<- xInvNew
+	getInverse <- function() xInv
+	list(set = set, get=get,
+		setInverse = setInverse,
+		getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Takes a special matrix object and either calculates its inverse
+## and stores it or retrieves a stored inverse if it exists
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    xInv <- x$getInverse()
+    if (!is.null(xInv)){
+    	message("Getting cached data")
+    	return(xInv)
+    }
+    xData <- x$get()
+    xInv <- solve(xData,...)
+    x$setInverse(xInv)
+    xInv
 }
